cad46ed29372e127de47acdaa910277ddef41333
Sakshi-lele <147378286+Sakshi-lele@users.noreply.github.com>
2025-05-09 12:42:45 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DesignPatternDemo
+{
+    public class OldPrinter
+    {
+        public void PrintOld()
+        {
+            Console.WriteLine("Old printing...");
+        }
+    }
+
+    public interface IPrinter
+    {
+        void Print();
+    }
+
+    public class PrintAdapter : IPrinter
+    {
+        private OldPrinter oldPrinter = new OldPrinter();
+
+        public void Print()
+        {
+            oldPrinter.PrintOld();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPrinter printer = new PrintAdapter();
+            printer.Print();
+        }
+    }
+}

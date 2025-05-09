2e3854da4d1cf4060eb3ddf51fa9bd01ecce23d5
Sakshi-lele <147378286+Sakshi-lele@users.noreply.github.com>
2025-05-09 06:49:28 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DesignPatternDemo
+{
+    public abstract class Prototype
+    {
+        public abstract Prototype Clone();
+    }
+
+    public class Person : Prototype
+    {
+        public string Name;
+        public override Prototype Clone() => (Prototype)this.MemberwiseClone();
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person { Name = "Sakshi" };
+            Person p2 = (Person)p1.Clone();
+
+            Console.WriteLine(p2.Name);
+        }
+    }
+}

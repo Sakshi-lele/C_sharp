ca93ed160228e9fbc0e65d053a280fe7de033443
Sakshi-lele <147378286+Sakshi-lele@users.noreply.github.com>
2025-05-09 06:27:56 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DesignPatternsDemo
+{
+    public sealed class Singleton
+    {
+        private static readonly Singleton _instance = new Singleton();
+        public static Singleton Instance => _instance;
+
+        private Singleton () { }
+
+        public void Log(string message) { 
+        Console.WriteLine(message); 
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton.Instance.Log("Hello from Singleton!");
+        }
+    }
+}

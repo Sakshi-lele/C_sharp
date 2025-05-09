4a42a96d14ecdc4548c4c771bf56628dea7a1789
Sakshi-lele <147378286+Sakshi-lele@users.noreply.github.com>
2025-05-09 07:09:28 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DesignPatternsDemo
+{
+    public class Car
+    {
+        public string Engine, Wheels;
+        public override string ToString() => $"Engine: {Engine}, Wheels: {Wheels}";
+    }
+
+    public class CarBuilder
+    {
+        private Car car = new Car();
+        public CarBuilder SetEngine (string engine)
+        {
+            car.Engine = engine;
+            return this ;
+        }
+        public CarBuilder SetWheels(string wheels)
+        {
+            car.Wheels = wheels;
+            return this ;
+        }
+        public Car Build() => car;
+    }
+
+    class Program
+    {
+        static void Main(string[] args) {
+            Car car = new CarBuilder()
+                .SetEngine("V8")
+                .SetWheels("Alloy")
+                .Build();
+
+            Console.WriteLine(car);
+        }
+    }
+
+}
